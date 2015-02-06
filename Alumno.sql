903326a167ff99705d5652a4ae3ea3cb77d2524d
AaronBautista <aaronbautista@hotmail.com>
2015-02-06 14:51:08 -0600

    Script SQL Actualizado

--- Alumno.sql
+++ Alumno.sql
@@ -3,27 +3,22 @@ create table tblObject
 IdObject int not null primary key key identity (1,1),
 Object nvarchar(max) null
 )
-
 create table tblAlumno
 (
-Id int not null primary key identity (1,1),
+Id int not null primary key,
 Nombre nvarchar(max) not null,
 ApPaterno nvarchar (max) not null,
 ApMaterno nvarchar(max) not null,
 Email nvarchar(max) not null
 )
-
-create procedure spInsertColumn
+create procedure spInsertColumn 
+@IdAlumno int
 as
 begin
-insert into tblAlumno(nombre,appaterno,apmaterno,email)
-select	[Object].value('(/Alumno/nombre)[1]','nvarchar(max)') as nombre,
-		[Object].value('(/Alumno/aPaterno)[1]','nvarchar(max)') as appaterno,
-		[Object].value('(/Alumno/aMaterno)[1]','nvarchar(max)') as apmaterno,
-		[Object].value('(/Alumno/email)[1]','nvarchar(max)') as email 
-		from tblObject where IdObject=(select max(IdObject) from tblObject)
+insert into tblAlumno (Id, Nombre, ApPaterno, ApMaterno, Email)
+select	@IdAlumno AS ID, [Object].value('(/Alumno/nombre)[1]','nvarchar(max)') as Nombre,
+		[Object].value('(/Alumno/aPaterno)[1]','nvarchar(max)') as ApPaterno,
+		[Object].value('(/Alumno/aMaterno)[1]','nvarchar(max)') as ApMaterno,
+		[Object].value('(/Alumno/email)[1]','nvarchar(max)') as Email
+		from tblObject where IdObject=@IdAlumno;
 end
-
-exec spInsertColumn
-select * from tblAlumno
-select * from tblObject
